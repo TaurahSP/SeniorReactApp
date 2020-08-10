dce6b2d47ad4f1dab68d30c1bf2fdd2b5f3723c5
Keshav Taurah <keshavtaurah@yahoo.com>
2020-08-09 22:18:40 -0700

    ErrorBoundry component added

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from '../components/CardList';
 import { robots } from '../robot';
 import SearchBox from "../components/SearchBox";
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry'
 import './App.css';
 
 const state = {
@@ -49,7 +50,9 @@ class App extends Component {
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
-                        <CardList robots={filteredRobots} />
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundry>
                     </Scroll>
 
                 </div>
