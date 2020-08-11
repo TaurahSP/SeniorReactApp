b21784dc0e3f951372eb08dba68a8efc70fbff99
Keshav Taurah <keshavtaurah@yahoo.com>
2020-08-10 22:41:39 -0700

    added constant file

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,7 @@
+
+import {CHANGE_SEARCH_FIELD} from "./constants";
+
 export const setSearchField = (text) => ({
-    type: 'CHANGE_SEARCH_FIELD',
+    type: CHANGE_SEARCH_FIELD,
     payload: text
 })
